35e4bb5f687e78b0afa405ff14eaef936768fdbe
Pierre-Adrien <pabuisson@gmail.com>
2018-05-21 16:31:25 +0200

    Part 2 : Routes and templates

--- README.md
+++ README.md
@@ -4,12 +4,14 @@ Ce dépôt regroupe tous les fichiers illustrant mes articles d'introduction à 
 [mon blog](https://blog.pabuisson.com):
 
 * [À la découverte de Sinatra #1 : présentation](https://blog.pabuisson.com/2014/05/a-la-decouverte-de-sinatra-1-hello-world/)
+* [À la découverte de Sinatra #2 : routes et templates](https://blog.pabuisson.com/2014/07/a-la-decouverte-de-sinatra-2-routes-et-templates/)
 
 ## Récupérer le source
 
 ```
 $ git clone git@bitbucket.org:pabuisson/blog-decouverte-sinatra.git
 $ git checkout -f partie-1
+$ git checkout -f partie-2
 ```
 
 ## Lancer l'application
--- app.rb
+++ app.rb
@@ -1,8 +1,79 @@
 # --- app.rb ---
 require 'sinatra'
+require 'sinatra/reloader' if development?
+require 'haml'
 
-# Lorsque l'utilisateur accède à la racine du projet, on affiche l'ERB "index.erb".
-# Par défaut, Sinatra cherche le template dans le répertoire "views" à la racine du projet
+#
+# Configuration de l'application
+#
+
+configure do
+  # Accessibles ensuite via settings.views et settings.lyrics
+  set :views,  File.join(settings.root, "app", "views")
+  set :lyrics, File.join(settings.root, "app", "data")
+end
+
+
+#
+# Méthodes accessibles à la fois dans les vues et dans le fichier app.rb
+#
+
+helpers do
+  class String
+    def url_encode
+      self.gsub ' ', '_'
+    end
+    def url_decode
+      self.gsub '_', ' '
+    end
+  end
+
+  def readfile(path)
+    s = ""
+    f = File.open(path, "r")
+    f.each_line { |line| s += line }
+    f.close
+    return s
+  end
+end
+
+
+#
+# --- Routes de l'application ---
+#
+
+# Lorsque l'utilisateur accède à la racine du projet, on affiche le haml "index.haml"
+# On liste les fichiers présents dans le répertoire settings.lyrics : chaque fichier
+# correspond à un titre de chanson
 get '/' do
-  erb :index
+  lyrics_path = File.join(settings.lyrics, "*.txt")
+  @lyrics = Dir.glob(lyrics_path)
+
+  haml :index
+end
+
+# Correspond aux URL "/song/my_way", "/song/fly_me_to_the_moon" etc.
+# On accède ensuite au paramètre :title avec le hash params
+get '/song/:title' do
+  # On construit le chemin du fichier contenant les paroles de la chanson à
+  # partir du titre passé en paramètre
+  lyrics_path = File.join(settings.lyrics, "#{params[:title].url_decode}.txt")
+
+  if File.exists?(lyrics_path)
+    # Si le fichier existe, on stocke son contenu dans la variable @lyrics
+    # et on affiche le template song.haml
+    @lyrics = readfile(lyrics_path)
+    haml :song
+  else
+    # Si le fichier n'existe pas, alors on renvoie une erreur 404
+    halt 404
+  end
+end
+
+
+
+# Si la page n'est pas trouvée, alors on plante de façon élégante en affichant une
+# page d'erreur 404 personnalisée
+not_found do
+  haml :err_404
 end
